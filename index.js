4d617c5fa058f8f3da302ed8b3a0f9186609d52d
sho5midday <yagi.shogo.1027@gmail.com>
2020-04-29 23:01:31 +0900

    07.モジュール化された処理

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
-
-module.exports = {
-}
+/**
+* 配列に含まれる整数が 17 で割り切れるものだけにする
+* @param {int} number
+*/
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+  }
+  
+  module.exports = {
+    isMultipleOfSeventeen
+  };
